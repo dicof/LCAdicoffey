fb7241d499661057816f9771e315b4c28bdb78a5
unknown <dicoffey@tcd.ie>
2019-10-16 13:26:11 +0100

    Started on the recursive element

--- LCA/node.py
+++ LCA/node.py
@@ -51,7 +51,28 @@ def lowestCommonAncestorDAG(root, x, y):
     if (root is not None):
         if (root.left is None and root.right is None and (x is not y)):
             return -1
-
+    return LCARecursive(root, x, y, [False], [False])
 
 
 def LCARecursive(root, x, y, path1, path2):
+    if (root is None):
+        return -1
+
+    if (root.visited is True):
+        return -1
+
+    left = LCARecursive(root.left, x, y, path1, path2)
+    right = LCARecursive(root.right, x, y, path1, path2)
+
+    if(root.val is x):
+        path1[0] = True
+        return root.val
+
+    if(root.val is y):
+        path2[0] = True
+        return root.val
+
+    if(path1[0] is True and path2[0] is True):
+        if (left is not -1 and right is not -1):
+            return root.val
+            
