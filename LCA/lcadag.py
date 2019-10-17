4c2440327001e44fca7c1d91f66cde4c48182691
unknown <dicoffey@tcd.ie>
2019-10-17 14:59:06 +0100

    Changed test code and corrected indent error in code

--- LCA/lcadag.py
+++ LCA/lcadag.py
@@ -7,32 +7,32 @@ class Node:
         self.pred = []
         self.succ = []
 
-    def dagLCA(root, x, y):
+def LCA(root, x, y):
 
-        if root is None:
-            return -1
+    if root is None:
+        return -1
 
-        if root.val == x or root.val == y:
-            return root
+    if root.val == x.val or root.val == y.val:
+        return root
 
-        if x == y:
-            return x.va;
-        lca = []
-        i = 0
-        while(i<len(x.pred)):
-            j = 0
-            while(j<len(y.pred)):
-                if(x.pred[i].val == y.pred[j].val):
-                    lca.append(x.pred[i].val)
-                    j+=1
-                else:
-                    j+=1
-            i+=1
-
-        if(lca == []):
-            if(x.val > y.val):
-                lca.append(dagLCA(root, x.pred[0], y))
+    if x == y:
+        return x.val;
+    lca = []
+    i = 0
+    while(i<len(x.pred)):
+        j = 0
+        while(j<len(y.pred)):
+            if(x.pred[i].val == y.pred[j].val):
+                lca.append(x.pred[i].val)
+                j+=1
             else:
-                lca.append(dagLCA(root,x,y,pred[0]))
+                j+=1
+        i+=1
 
-        return max(lca)
+    if(lca == []):
+        if(x.val > y.val):
+            lca.append(dagLCA(root, x.pred[0], y))
+        else:
+            lca.append(dagLCA(root,x,y,pred[0]))
+
+    return max(lca)
--- LCA/lcadagtest.py
+++ LCA/lcadagtest.py
@@ -5,28 +5,35 @@ import lcadag
 
 class test_lcadag(unittest.TestCase):
 
-    root = Node.Node(1)
-    r2 = Node.Node(2)
-    r3 = Node.Node(3)
-    r4 = Node.Node(4)
-    r5 = Node.Node(5)
-    r6 = Node.Node(6)
-    root.succ = [r2,r3,r4,r5]
-    r2.succ = [r4]
-    r2.pred = [root]
-    r3.succ = [r4, r5]
-    r3.pred = [root]
-    r4.succ = [r5]
-    r4.pred = [r2,r3,root]
-    r5.pred = [r3,r4,root]
-    r6.pred = [r4]
+
 
     def test_basicTree(self):
+        root = lcadag.Node(1)
+        r2 = lcadag.Node(2)
+        r3 = lcadag.Node(3)
+        r4 = lcadag.Node(4)                               #              ----1-----
+        r5 = lcadag.Node(5)                                         #  /   /   \   \
+        r6 = lcadag.Node(6)                                        # 2    /    3   \
+        root.succ = [r2,r3,r4,r5]                               #   \ /      /\  \
+        r2.succ = [r4]                                      #        4------- \  /
+        r2.pred = [root]                                    #        \---------5
+        r3.succ = [r4, r5]
+        r3.pred = [root]
+        r4.succ = [r5]
+        r4.pred = [r2,r3,root]
+        r5.pred = [r3,r4,root]
+        r6.pred = [r4]
+        self.assertEqual(lcadag.LCA(root,r2, r3), 1, "Should be 1")
+        self.assertEqual(lcadag.LCA(root,r1, r5), 1, "Should be 1")
+        self.assertEqual(lcadag.LCA(root,r4, r3), 3, "Should be 3")
+        self.assertEqual(lcadag.LCA(root,r5, r2), 2, "Should be 2")
 
-    def test_nullTree(self):
 
-    def test_invalidNode(self):
+    # def test_nullTree(self):
+    #
+    # def test_invalidNode(self):
+    #
+    # def test_LCAisNode(self):
 
-    def test_LCAisNode(self):
-
-    
+if __name__ == '__main__':
+    unittest.main()
