a3a8b2c92a64b798c94d81715c28d588a5f64afb
unknown <dicoffey@tcd.ie>
2019-10-18 14:25:43 +0100

    Corrected my tests to accomodate new solution

--- LCA/lca.py
+++ LCA/lca.py
@@ -58,24 +58,14 @@ def createDAG():
 
 class test_node(unittest.TestCase):
 
-    #
-    # def test_BasicTree(self):
-    #     root = node.Node(1)                      #  1
-    #     root.left = node.Node(2)                # /   \
-    #     root.right = node.Node(3)              # 2     3
-    #     root.left.left = node.Node(4)         #/  \   / \
-    #     root.left.right = node.Node(5)       #4   5  6  7
-    #     root.right.left = node.Node(6)
-    #     root.right.right = node.Node(7)
-    #     self.assertEqual(2, node.lowestCommonAncestor(root, 4, 5))
-    #     self.assertEqual(1, node.lowestCommonAncestor(root, 6, 5))
-    #
-    # def test_NullTree(self):
-    #     root = None
-    #     self.assertEqual(-1, node.lowestCommonAncestor(root, 4, 5), 'Empty tree returns -1')
-    #     self.assertEqual(-1, node.lowestCommonAncestor(None, 0, 0), 'Empty tree returns -1')
-    #
-    #
+
+
+    def test_NullTree(self):
+        root = None
+        self.assertEqual(-1, node.LCA(root, 4, 5), 'Empty tree returns -1')
+        self.assertEqual(-1, node.LCA(None, 0, 0), 'Empty tree returns -1')
+
+
     # def test_InvalidNode(self):
     #     root = node.Node(1)
     #     root.left = node.Node(2)
@@ -84,108 +74,83 @@ class test_node(unittest.TestCase):
     #     root.left.right = node.Node(5)
     #     root.right.left = node.Node(6)
     #     root.right.right = node.Node(7)
-    #     self.assertEqual(-1, node.lowestCommonAncestor(root, 4, 8), "Unfound node returns -1")
-    #
-    # def test_CommonAncestorIsNode(self):
-    #     root = node.Node(1)
-    #     root.left = node.Node(2)
-    #     root.right = node.Node(3)
-    #     root.left.left = node.Node(4)
-    #     root.left.right = node.Node(5)
-    #     root.right.left = node.Node(6)
-    #     root.right.right = node.Node(7)
-    #     self.assertEqual(2, node.lowestCommonAncestor(root, 2, 4), "Common Ancestor of 2 & 4 is 2 itself")
-    #     self.assertEqual(2, node.lowestCommonAncestor(root, 2, 2), "Common Ancestor of 2 & 2 is 2 itself")
-    #     self.assertEqual(2, node.lowestCommonAncestor(root, 4, 2), "Common Ancestor of 4 & 2 is 2 itself")
-    #
-    # def test_ExtremeInputs(self):
-    #     root = node.Node(-1000000000000000)
-    #     root.left = node.Node(2.5)
-    #     root.right = node.Node(-3.3)
-    #     self.assertEqual(-1000000000000000, node.lowestCommonAncestor(root, 2.5, -3.3), "Common ancestor -1000000000000000")
-    #     root.left.left = node.Node('seven')
-    #     root.left.right = node.Node('crazy')
-    #     self.assertEqual(2.5, node.lowestCommonAncestor(root, 'seven', 'crazy'), "Common ancestor 2.5")
-    #
-    #
-    # def test_LeftLeaningTree(self):
-    #     # testing a left leaning tree
-    #     #
-    #     #                         1
-    #     #                       /
-    #     #                     2
-    #     #                   /
-    #     #                 3
-    #     #               /
-    #     #             4
-    #     #           /
-    #     #         5
-    #     #       /
-    #     #     6
-    #     #   /
-    #     # 7
-    #     root = node.Node(1)
-    #     root.left = node.Node(2)
-    #     root.left.left = node.Node(3)
-    #     root.left.left.left = node.Node(4)
-    #     root.left.left.left.left = node.Node(5)
-    #     root.left.left.left.left.left = node.Node(6)
-    #     root.left.left.left.left.left.left = node.Node(7)
-    #
-    #     self.assertEqual(node.lowestCommonAncestor(root, 1, 2), 1, "Should return 1")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 7, 2), 2, "Should return 2")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 4, 6), 4, "Should return 4")
-    #
-    # def test_RightLeaningTree(self):
-    #     #   testing a right leaning tree
-    #     #
-    #     #   1
-    #     #    \
-    #     #     2
-    #     #      \
-    #     #       3
-    #     #        \
-    #     #         4
-    #     #          \
-    #     #           5
-    #     #            \
-    #     #             6
-    #     #              \
-    #     #               7
-    #     root = node.Node(1)
-    #     root.right = node.Node(2)
-    #     root.right.right = node.Node(3)
-    #     root.right.right.right = node.Node(4)
-    #     root.right.right.right.right = node.Node(5)
-    #     root.right.right.right.right.right = node.Node(6)
-    #     root.right.right.right.right.right.right = node.Node(7)
-    #
-    #     self.assertEqual(node.lowestCommonAncestor(root, 1, 2), 1, "Should return 1")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 7, 2), 2, "Should return 2")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 4, 6), 4, "Should return 4")
-    #
-    # def test_TreeOfOneElement(self):
-    #     # create one element tree
-    #     # test one element tree
-    #     root = node.Node(1)
-    #
-    #     self.assertEqual(node.lowestCommonAncestor(root, 1, 2), -1, "Should return -1")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 1, 1), 1, "Should return 1")
-    #     self.assertEqual(node.lowestCommonAncestor(root, 6, 7), -1, "Should return -1")
-    #
-    #
-    #
-    # def test_DAG(self):
-    #     root = node.Node(1)                      #  1
-    #     root.left = node.Node(2)                # /   \
-    #     root.right = node.Node(3)              # 2     3
-    #     root.left.right = node.Node(4)        # / \  /  \
-    #     root.right.left = root.left.right  #       4
-    #
-    #
-    #     self.assertEqual(node.lowestCommonAncestor(root, 4, 3), 3, "Should be 3")
-    #
-    #
+    #     self.assertEqual(-1, node.LCA(root, 4, 8), "Unfound node returns -1")
+
+    def test_CommonAncestorIsNode(self):
+        root = createTree()
+        self.assertEqual(2, node.LCA(root, 2, 4), "Common Ancestor of 2 & 4 is 2 itself")
+        self.assertEqual(2, node.LCA(root, 2, 2), "Common Ancestor of 2 & 2 is 2 itself")
+        self.assertEqual(2, node.LCA(root, 4, 2), "Common Ancestor of 4 & 2 is 2 itself")
+
+    def test_ExtremeInputs(self):
+        root = node.Node(-1000000000000000)
+        n1 = node.Node(2.5)
+        n2 = node.Node(-3.3)
+        root.addChild(n1)
+        root.addChild(n2)
+        self.assertEqual(-1000000000000000, node.LCA(root, 2.5, -3.3), "Common ancestor -1000000000000000")
+        n3 = node.Node('seven')
+        n4 = node.Node('crazy')
+        self.assertEqual(2.5, node.LCA(root, 'seven', 'crazy'), "Common ancestor 2.5")
+
+
+    def test_LeftLeaningTree(self):
+        # testing a long single branched tree
+        #
+        #                         1
+        #                       /
+        #                     2
+        #                   /
+        #                 3
+        #               /
+        #             4
+        #           /
+        #         5
+        #       /
+        #     6
+        #   /
+        # 7
+        root = node.Node(1)
+        n2 = node.Node(2)
+        n3 = node.Node(3)
+        n4 = node.Node(4)
+        n5 = node.Node(5)
+        n6 = node.Node(6)
+        n7 = node.Node(7)
+        root.addChild(n2)
+        n2.addChild(n3)
+        n3.addChild(n4)
+        n4.addChild(n5)
+        n5.addChild(n6)
+        n6.addChild(n7)
+
+        self.assertEqual(node.LCA(root, 1, 2), 1, "Should return 1")
+        self.assertEqual(node.LCA(root, 7, 2), 2, "Should return 2")
+        self.assertEqual(node.LCA(root, 4, 6), 4, "Should return 4")
+    
+
+    def test_TreeOfOneElement(self):
+        # create one element tree
+        # test one element tree
+        root = node.Node(1)
+
+        self.assertEqual(node.LCA(root, 1, 2), -1, "Should return -1")
+        self.assertEqual(node.LCA(root, 1, 1), 1, "Should return 1")
+        self.assertEqual(node.LCA(root, 6, 7), -1, "Should return -1")
+
+
+
+    def test_DAG(self):
+        root = node.Node(1)                      #  1
+        root.left = node.Node(2)                # /   \
+        root.right = node.Node(3)              # 2     3
+        root.left.right = node.Node(4)        # / \  /  \
+        root.right.left = root.left.right  #       4
+
+
+        self.assertEqual(node.LCA(root, 4, 3), 3, "Should be 3")
+
+
 
     def test_basicTree(self):
         root = createTree()
--- LCA/node.py
+++ LCA/node.py
@@ -55,13 +55,6 @@ def LCA(root, x, y):
 
     return deepestAncestor.val
 
-
-
-
-
-
-
-
 def findNode(node, val):
     if node.val == val:
         return node
@@ -72,79 +65,95 @@ def findNode(node, val):
                 return n
     else:
         return None
+
+
+
+
+#
+#
+# def findNode(node, val):
+#     if node.val == val:
+#         return node
+#     elif len(node.children) != 0:
+#         for x in node.children:
+#             n = findNode(x,val)
+#             if n != None:
+#                 return n
+#     else:
+#         return None
+# # def lowestCommonAncestor(root, x, y):
+# #
+# #     path1 = []
+# #     path2 = []
+# #
+# #     if (not findPath(root, path1, x) or not findPath(root, path2, y)):
+# #         return -1
+# #
+# #
+# #     i = 0
+# #     while(i < len(path1) and i < len(path2)):
+# #         if path1[i] != path2[i]:
+# #             break
+# #         i += 1
+# #     return path1[i-1]
+#
+#
+# def findPath( root, path, k):
+#
+#     if root is None:
+#         return False
+#
+#     path.append(root.val)
+#
+#     if root.val == k :
+#         return True
+#
+#     if ((root.left != None and findPath(root.left, path, k)) or
+#             (root.right!= None and findPath(root.right, path, k))):
+#         return True
+#
+#
+#     path.pop()
+#     return False
+#
+#
 # def lowestCommonAncestor(root, x, y):
+#     if (root is None):
+#         return -1
+#     else:
+#         if (root.left is None and root.right is None and (x is not y)):
+#             return -1
+#     return LCARecursive(root, x, y, [False], [False])
 #
-#     path1 = []
-#     path2 = []
 #
-#     if (not findPath(root, path1, x) or not findPath(root, path2, y)):
+# def LCARecursive(root, x, y, path1, path2):
+#     if (root is None):
+#         return -1
+#     if (root.visited is True):
 #         return -1
 #
+#     left = LCARecursive(root.left, x, y, path1, path2)
+#     right = LCARecursive(root.right, x, y, path1, path2)
 #
-#     i = 0
-#     while(i < len(path1) and i < len(path2)):
-#         if path1[i] != path2[i]:
-#             break
-#         i += 1
-#     return path1[i-1]
-
-
-def findPath( root, path, k):
-
-    if root is None:
-        return False
-
-    path.append(root.val)
-
-    if root.val == k :
-        return True
-
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right!= None and findPath(root.right, path, k))):
-        return True
-
-
-    path.pop()
-    return False
-
-
-def lowestCommonAncestor(root, x, y):
-    if (root is None):
-        return -1
-    else:
-        if (root.left is None and root.right is None and (x is not y)):
-            return -1
-    return LCARecursive(root, x, y, [False], [False])
-
-
-def LCARecursive(root, x, y, path1, path2):
-    if (root is None):
-        return -1
-    if (root.visited is True):
-        return -1
-
-    left = LCARecursive(root.left, x, y, path1, path2)
-    right = LCARecursive(root.right, x, y, path1, path2)
-
-    if(root.val is x):
-        path1[0] = True
-        return root.val
-
-    if(root.val is y):
-        path2[0] = True
-        return root.val
-
-    if(path1[0] is True and path2[0] is True):
-        if (left is not -1 and right is not -1):
-            return root.val
-        elif(left is not -1):
-            return left
-        else:
-            return right
-
-    if (left is not -1):
-        return left
-    if(right is not -1):
-        return right
-
-    return -1
+#     if(root.val is x):
+#         path1[0] = True
+#         return root.val
+#
+#     if(root.val is y):
+#         path2[0] = True
+#         return root.val
+#
+#     if(path1[0] is True and path2[0] is True):
+#         if (left is not -1 and right is not -1):
+#             return root.val
+#         elif(left is not -1):
+#             return left
+#         else:
+#             return right
+#
+#     if (left is not -1):
+#         return left
+#     if(right is not -1):
+#         return right
+#
+#     return -1
